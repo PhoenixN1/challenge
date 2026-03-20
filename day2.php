cc36781c5e827fb8a8a628dcce9f7c7cb6e68c15
Mostafa Mellouki <mostafamellouki6@gmail.com>
2026-03-20 20:28:26 +0100

    Add files via upload

---
+++ day2.php
@@ -0,0 +1,23 @@
+<?php 
+echo "<h1>Variables dans php :</h1>";
+
+$nom = "Mostafa";
+$age = 19;
+$ville = "Meknes";
+
+
+echo "$nom";
+echo $age;
+echo $ville;
+
+$numbrepostive = 90;
+$numbrenegative = -2;
+$numbredecimal = 3.5;
+
+
+echo "<br>";
+echo "<br>";
+echo"nombre positive : $numbrepostive";
+echo "<br>";
+echo"nombre positive : $numbredecimal";
+?>
