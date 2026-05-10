0a426b085b50523bca32cad5d2443e2d39d1584d
Mostafa Mellouki <mostafamellouki6@gmail.com>
2026-05-10 19:09:40 +0100

    Implement CRUD operations for students in PHP

--- day34.php
+++ day34.php
@@ -1,1 +1,34 @@
-no code
+<?php
+$conn = mysqli_connect("localhost", "root", "", "ecole");
+
+if (!$conn) {
+    die("Connexion échouée");
+}
+
+if (isset($_POST['update'])) {
+    $id = $_POST['id'];
+    $nom = $_POST['nom'];
+    $prenom = $_POST['prenom'];
+    $age = $_POST['age'];
+
+    $sql = "UPDATE etudiants SET nom='$nom', prenom='$prenom', age='$age' WHERE id='$id'";
+    mysqli_query($conn, $sql);
+}
+
+if (isset($_POST['delete'])) {
+    $id = $_POST['id'];
+
+    $sql = "DELETE FROM etudiants WHERE id='$id'";
+    mysqli_query($conn, $sql);
+}
+?>
+
+<form method="POST">
+    <input type="number" name="id" placeholder="ID" required><br><br>
+    <input type="text" name="nom" placeholder="Nom"><br><br>
+    <input type="text" name="prenom" placeholder="Prénom"><br><br>
+    <input type="number" name="age" placeholder="Âge"><br><br>
+
+    <button type="submit" name="update">UPDATE</button>
+    <button type="submit" name="delete">DELETE</button>
+</form>
