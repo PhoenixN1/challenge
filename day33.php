cc5306613d8efe8c4ff4f4961f01be086a7cd685
Mostafa Mellouki <mostafamellouki6@gmail.com>
2026-05-10 19:10:24 +0100

    Add PHP script to connect and query users table

--- day33.php
+++ day33.php
@@ -1,1 +1,25 @@
-no code
+```php
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbname = "test";
+
+$conn = new mysqli($host, $user, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed");
+}
+
+$sql = "SELECT * FROM users";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo $row["id"] . " " . $row["name"] . "<br>";
+    }
+}
+
+$conn->close();
+?>
+```
