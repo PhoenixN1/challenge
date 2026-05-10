8ecb3ef1f4b9adc4a42a975dbbdc828b81eb16a4
Mostafa Mellouki <mostafamellouki6@gmail.com>
2026-05-10 19:07:59 +0100

    Add PHP script to display student list from database

--- day36.php
+++ day36.php
@@ -1,1 +1,40 @@
-nocode
+<?php
+$conn = new mysqli("localhost", "root", "", "school");
+
+if ($conn->connect_error) {
+    die("Connexion échouée");
+}
+
+$sql = "SELECT * FROM students";
+$result = $conn->query($sql);
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Liste des étudiants</title>
+</head>
+<body>
+
+<h2>Liste des étudiants</h2>
+
+<?php if ($result->num_rows > 0): ?>
+    <?php while ($row = $result->fetch_assoc()): ?>
+        <div>
+            <p>ID: <?php echo $row['id']; ?></p>
+            <p>Nom: <?php echo $row['name']; ?></p>
+            <p>Email: <?php echo $row['email']; ?></p>
+            <hr>
+        </div>
+    <?php endwhile; ?>
+<?php else: ?>
+    <p>Aucun étudiant trouvé</p>
+<?php endif; ?>
+
+</body>
+</html>
+
+<?php
+$conn->close();
+?>
