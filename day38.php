daa9b07558d43a3100f393056571cf7acd7f905a
Mostafa Mellouki <mostafamellouki6@gmail.com>
2026-05-10 19:06:15 +0100

    Update day38.php

--- day38.php
+++ day38.php
@@ -1,1 +1,50 @@
-3dna
+<?php
+$conn = new mysqli("localhost", "root", "", "school");
+
+if ($conn->connect_error) {
+    die("Connexion échouée");
+}
+
+if (isset($_POST['submit'])) {
+    $nom = $_POST['nom'];
+    $prenom = $_POST['prenom'];
+    $age = $_POST['age'];
+    $email = $_POST['email'];
+
+    $sql = "INSERT INTO etudiants (nom, prenom, age, email) 
+            VALUES ('$nom', '$prenom', '$age', '$email')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "Données ajoutées avec succès";
+    } else {
+        echo "Erreur";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Ajouter Étudiant</title>
+</head>
+<body>
+
+<form method="POST">
+    <label>Nom :</label><br>
+    <input type="text" name="nom" required><br><br>
+
+    <label>Prénom :</label><br>
+    <input type="text" name="prenom" required><br><br>
+
+    <label>Âge :</label><br>
+    <input type="number" name="age" required><br><br>
+
+    <label>Email :</label><br>
+    <input type="email" name="email" required><br><br>
+
+    <button type="submit" name="submit">Ajouter</button>
+</form>
+
+</body>
+</html>
