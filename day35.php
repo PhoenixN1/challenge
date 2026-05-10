bb9ebfb84edcd17d015b19142a95251aa3c0b8c3
Mostafa Mellouki <mostafamellouki6@gmail.com>
2026-05-10 19:08:51 +0100

    Implement PDO connection in day35.php
    Added PDO connection handling alongside mysqli.

--- day35.php
+++ day35.php
@@ -1,1 +1,33 @@
-nocode
+<?php
+
+$host = "localhost";
+$dbname = "nom_base";
+$username = "root";
+$password = "";
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connexion échouée : " . $conn->connect_error);
+}
+
+echo "Connexion réussie";
+
+?>
+<?php
+
+$host = "localhost";
+$dbname = "nom_base";
+$username = "root";
+$password = "";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Connexion réussie";
+} catch (PDOException $e) {
+    die("Connexion échouée : " . $e->getMessage());
+}
+
+?>
